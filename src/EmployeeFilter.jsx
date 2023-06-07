024e9f5dc7f84fb0f6b398f5873b459cd7c35635
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-06-07 14:44:45 -0700

    update

--- src/EmployeeFilter.jsx
+++ src/EmployeeFilter.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 export default function EmployeeFilter () {
     const navigate = useNavigate()
+    const [ searchParams ] = useSearchParams()
     return (
         <div>
             Currently Employed:
             {' '}
             <select 
+                value={searchParams.get('employed') || ''}
                 onChange={(e) => navigate(
                     e.target.value ?
                     `/employees?employed=${e.target.value}` :
